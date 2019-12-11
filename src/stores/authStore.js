4c83c1b93b8613f2a8d440e2b11fd5623519b2e6
Luiz Américo <luizamericop@gmail.com>
2019-12-11 06:29:36 -0300

    Add prettier script and format code accordingly

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,67 +1,85 @@
-import { observable, action } from 'mobx';
-import agent from '../agent';
-import userStore from './userStore';
-import commonStore from './commonStore';
+import { observable, action } from 'mobx'
+import agent from '../agent'
+import userStore from './userStore'
+import commonStore from './commonStore'
 
 class AuthStore {
-  @observable inProgress = false;
-  @observable errors = undefined;
+  @observable inProgress = false
+  @observable errors = undefined
 
   @observable values = {
     username: '',
     email: '',
-    password: '',
-  };
+    password: ''
+  }
 
   @action setUsername(username) {
-    this.values.username = username;
+    this.values.username = username
   }
 
   @action setEmail(email) {
-    this.values.email = email;
+    this.values.email = email
   }
 
   @action setPassword(password) {
-    this.values.password = password;
+    this.values.password = password
   }
 
   @action reset() {
-    this.values.username = '';
-    this.values.email = '';
-    this.values.password = '';
+    this.values.username = ''
+    this.values.email = ''
+    this.values.password = ''
   }
 
   @action login() {
-    this.inProgress = true;
-    this.errors = undefined;
+    this.inProgress = true
+    this.errors = undefined
     return agent.Auth.login(this.values.email, this.values.password)
       .then(({ user }) => commonStore.setToken(user.token))
       .then(() => userStore.pullUser())
-      .catch(action((err) => {
-        this.errors = err.response && err.response.body && err.response.body.errors;
-        throw err;
-      }))
-      .finally(action(() => { this.inProgress = false; }));
+      .catch(
+        action(err => {
+          this.errors =
+            err.response && err.response.body && err.response.body.errors
+          throw err
+        })
+      )
+      .finally(
+        action(() => {
+          this.inProgress = false
+        })
+      )
   }
 
   @action register() {
-    this.inProgress = true;
-    this.errors = undefined;
-    return agent.Auth.register(this.values.username, this.values.email, this.values.password)
+    this.inProgress = true
+    this.errors = undefined
+    return agent.Auth.register(
+      this.values.username,
+      this.values.email,
+      this.values.password
+    )
       .then(({ user }) => commonStore.setToken(user.token))
       .then(() => userStore.pullUser())
-      .catch(action((err) => {
-        this.errors = err.response && err.response.body && err.response.body.errors;
-        throw err;
-      }))
-      .finally(action(() => { this.inProgress = false; }));
+      .catch(
+        action(err => {
+          this.errors =
+            err.response && err.response.body && err.response.body.errors
+          throw err
+        })
+      )
+      .finally(
+        action(() => {
+          this.inProgress = false
+        })
+      )
   }
 
   @action logout() {
-    commonStore.setToken(undefined);
-    userStore.forgetUser();
-    return Promise.resolve();
+    commonStore.setToken(undefined)
+    userStore.forgetUser()
+    return Promise.resolve()
   }
 }
 
-export default new AuthStore();
+export default new AuthStore()
--- src/stores/editorStore.js
+++ src/stores/editorStore.js
@@ -1,82 +1,99 @@
-import { observable, action } from 'mobx';
-import articlesStore from './articlesStore';
+import { observable, action } from 'mobx'
+import articlesStore from './articlesStore'
 
 class EditorStore {
+  @observable inProgress = false
+  @observable errors = undefined
+  @observable articleSlug = undefined
 
-  @observable inProgress = false;
-  @observable errors = undefined;
-  @observable articleSlug = undefined;
-
-  @observable title = '';
-  @observable description = '';
-  @observable body = '';
-  @observable tagList = [];
+  @observable title = ''
+  @observable description = ''
+  @observable body = ''
+  @observable tagList = []
 
   @action setArticleSlug(articleSlug) {
     if (this.articleSlug !== articleSlug) {
-      this.reset();
-      this.articleSlug = articleSlug;
+      this.reset()
+      this.articleSlug = articleSlug
     }
   }
 
   @action loadInitialData() {
-    if (!this.articleSlug) return Promise.resolve();
-    this.inProgress = true;
-    return articlesStore.loadArticle(this.articleSlug, { acceptCached: true })
-      .then(action((article) => {
-        if (!article) throw new Error('Can\'t load original article');
-        this.title = article.title;
-        this.description = article.description;
-        this.body = article.body;
-        this.tagList = article.tagList;
-      }))
-      .finally(action(() => { this.inProgress = false; }));
+    if (!this.articleSlug) return Promise.resolve()
+    this.inProgress = true
+    return articlesStore
+      .loadArticle(this.articleSlug, { acceptCached: true })
+      .then(
+        action(article => {
+          if (!article) throw new Error("Can't load original article")
+          this.title = article.title
+          this.description = article.description
+          this.body = article.body
+          this.tagList = article.tagList
+        })
+      )
+      .finally(
+        action(() => {
+          this.inProgress = false
+        })
+      )
   }
 
   @action reset() {
-    this.title = '';
-    this.description = '';
-    this.body = '';
-    this.tagList = [];
+    this.title = ''
+    this.description = ''
+    this.body = ''
+    this.tagList = []
   }
 
   @action setTitle(title) {
-    this.title = title;
+    this.title = title
   }
 
   @action setDescription(description) {
-    this.description = description;
+    this.description = description
   }
 
   @action setBody(body) {
-    this.body = body;
+    this.body = body
   }
 
   @action addTag(tag) {
-    if (this.tagList.includes(tag)) return;
-    this.tagList.push(tag);
+    if (this.tagList.includes(tag)) return
+    this.tagList.push(tag)
   }
 
   @action removeTag(tag) {
-    this.tagList = this.tagList.filter(t => t !== tag);
+    this.tagList = this.tagList.filter(t => t !== tag)
   }
 
   @action submit() {
-    this.inProgress = true;
-    this.errors = undefined;
+    this.inProgress = true
+    this.errors = undefined
     const article = {
       title: this.title,
       description: this.description,
       body: this.body,
       tagList: this.tagList,
-      slug: this.articleSlug,
-    };
-    return (this.articleSlug ? articlesStore.updateArticle(article) : articlesStore.createArticle(article))
-      .catch(action((err) => {
-        this.errors = err.response && err.response.body && err.response.body.errors; throw err;
-      }))
-      .finally(action(() => { this.inProgress = false; }));
+      slug: this.articleSlug
+    }
+    return (this.articleSlug
+      ? articlesStore.updateArticle(article)
+      : articlesStore.createArticle(article)
+    )
+      .catch(
+        action(err => {
+          this.errors =
+            err.response && err.response.body && err.response.body.errors
+          throw err
+        })
+      )
+      .finally(
+        action(() => {
+          this.inProgress = false
+        })
+      )
   }
 }
 
-export default new EditorStore();
+export default new EditorStore()
